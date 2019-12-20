b38fc45f5d64f9356d49320d192a34d5690e5737
billalp <billal.patel@rightmove.co.uk>
2019-12-20 16:38:19 +0000

    Handling errors

--- routes/places.js
+++ routes/places.js
@@ -29,25 +29,29 @@ const dummyPlaces = [
   }
 ];
 
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid;
+  const user = dummyPlaces.find((element) => element.creatorId === userId);
+
+  if (!user) {
+    const error = new Error('Could not find place with this user ID.');
+    error.code = 404;
+    return next(error);
+  }
+  res.json({ user });
+});
+
 router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid;
   const place = dummyPlaces.find((element) => element.id === placeId);
 
   if (!place) {
-    return res.status(404).json({
-      errorMessage: 'Could not find place.'
-    });
+    const error = new Error('Could not find searched place.');
+    error.code = 404;
+    throw error;
   }
 
   res.json({ place });
-});
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const user = dummyPlaces.find((element) => element.creatorId === userId);
-  res.json({
-    user
-  });
 });
 
 module.exports = router;
