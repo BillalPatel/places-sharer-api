af15e3bcb762e973f8f23c8abc9a3ec8b69cdc75
billalp <billal.patel@rightmove.co.uk>
2019-12-30 17:47:25 +0000

    Create user

--- controllers/users.js
+++ controllers/users.js
@@ -1,32 +1,49 @@
 const uuid = require('uuid/v4');
 const { validationResult } = require('express-validator');
+
+const User = require('../models/user-mongoose');
 const HttpError = require('../models/http-error');
 
 const getUsers = (req, res, next) => {
   res.status(200).json({ users: dummyUsers });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     throw new HttpError('Invalid input submitted. Try submitting something else', 422);
   }
 
   const { name, email, password } = req.body;
-  const userExists = dummyUsers.find((element) => element.email === email);
 
-  if (userExists) {
-    throw new HttpError('User email already exists. Try logging in instead', 401);
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return next(new HttpError('Error occurred when signing up', 500));
+  }
+
+  if (existingUser) {
+    return next(new HttpError('User already exists. Try logging in instead', 422));
   }
 
   const createdUser = {
     id: uuid(),
     name,
-    password
+    email,
+    password,
+    imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/d/d6/London-Eye-2009.JPG',
+    places
   };
-  dummyUsers.push(createdUser);
 
-  res.status(201).json({ user: createdUser });
+  try {
+    await createdUser.save();
+  } catch (error) {
+    return next(new HttpError('Could not create the new user', 500));
+  }
+
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
@@ -36,7 +53,7 @@ const login = (req, res, next) => {
   if (!foundUser || !foundUser.password === password) {
     throw new HttpError('Could not find user', 422);
   } else {
-    res.status(200).json({ meessage: 'Logged in successfully' });
+    res.status(200).json({ message: 'Logged in successfully' });
   }
 };
 
