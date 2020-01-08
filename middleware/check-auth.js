5690a95c729ba39a9118ff30194ad55650140405
billalp <billal.patel@rightmove.co.uk>
2020-01-08 16:15:55 +0000

    Token comparison

---
+++ middleware/check-auth.js
@@ -0,0 +1,16 @@
+const HttpError = require('../models/http-error');
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+      return next(new HttpError('Authentication failed for this user', 401));
+    }
+    const decodedToken = jwt.verify(token, 'super');
+    req.userData = { userData: decodedToken.userId };
+    next();
+  } catch (error) {
+    return next(new HttpError('Authentication failed', 401));
+  }
+};
