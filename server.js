f8c0619541fab6d3a9eeb7de68f36190f3d418fe
billalp <billal.patel@rightmove.co.uk>
2020-01-10 15:30:54 +0000

    Testing env

--- server.js
+++ server.js
@@ -49,8 +49,9 @@ app.use((error, req, res, next) => {
 //   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-w1qcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 //   .then(() => {
     app.listen(process.env.PORT || 5000);
+    console.log(process.env);
     console.log('*App Running*');
   // })
   // .catch((error) => {
-    // console.log('Build failed... ', error);
+  //   console.log('Build failed... ', error);
   // });
