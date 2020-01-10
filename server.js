976511572c82ac301a93effe0eab4ccdbba4c013
billalp <billal.patel@rightmove.co.uk>
2020-01-10 14:42:10 +0000

    Attempt at getting it to work

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ mongoose
   .connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-w1qcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(process.env.PORT || 5000);
-    console.log('*App Running*');
+    console.log('**App Running**');
   })
   .catch((error) => {
     console.log('Build failed... ', error);
