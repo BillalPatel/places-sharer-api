eb415893daa121032bdb3283f12523e57b375022
billalp <billal.patel@rightmove.co.uk>
2020-01-10 15:44:16 +0000

    To master

--- utils/location.js
+++ utils/location.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 const HttpError = require('../models/http-error');
-const API_KEY = require('./location-api');
+// const API_KEY = require('./location-api');
 
-const googleMapsKey = API_KEY.API_KEY;
+const googleMapsKey = process.env.GOOGLE_API_KEY;
 
 const getAddressCoordinate = async (address) => {
   const mapsResponse = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=
